ba96602d5bd72ca0b86421fa500fc4c61442f483
OmAvhad <89376666+OmAvhad@users.noreply.github.com>
2024-09-01 21:38:38 +0530

    close drawer

--- vibranium-server/src/db.js
+++ vibranium-server/src/db.js
@@ -3,6 +3,7 @@ import { MONGODB_URI } from "./config.js";
 
 export const connectDB = async () => {
   try {
+    console.log(MONGODB_URI);
     await mongoose.connect(MONGODB_URI);
     console.log("MongoDB is connected");
   } catch (error) {
