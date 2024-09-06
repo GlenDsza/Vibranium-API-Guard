6a9f956431d86f47142631d98424412c6a53e84a
Kenneth <Kenneth012004@outlook.com>
2024-09-06 11:55:55 +0530

    vibranium-shield: FIx minor bugs.

--- fast-api/main.py
+++ fast-api/main.py
@@ -190,4 +190,4 @@ async def get_order(orderId: str = Path(...)):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
